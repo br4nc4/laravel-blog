46b2d5b147e11b91e5ca315b695a36ca4eba8f2e
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-09-01 15:24:33 +0200

    configuration vue router

---
+++ resources/js/router.js
@@ -0,0 +1,27 @@
+//deve comunicare a vue che vogliamo usare questa libreria
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import Home from "./pages/Home.vue";
+import About from "./pages/About.vue";
+import Contact from "./pages/Contact.vue";
+
+Vue.use(VueRouter)
+
+// dobbiamo creare un array con le rotte
+const routes = [
+    /* 
+        path è uguale all'URI da scrivere nella barra degli indirizzi
+        component è il componente da mostrare quando la pagina viene visualizzata
+    */
+    {path: "/", component: Home, name:"home.index"},
+    {path: "/about", component: About, name:"about.index"},
+    {path: "/contact", component: Contact, name:"contact.index"},
+]
+
+//dobbiamo esportare un istanza di VueRouter() con le eventuali configurazioni
+export default new VueRouter({
+    //routes è un array di oggetti che rappresenta la configurazione di ogni rotta
+    routes,
+    mode: "history",
+}) 
