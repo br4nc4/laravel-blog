3b75739a584ca29d8609a154a7c0e672d90b1ddb
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-09-01 16:01:50 +0200

    route links and route any

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -4,6 +4,8 @@
         <TheNavbar></TheNavbar>
 
         <!-- contenuto di ogni pagina -->
+        <router-view></router-view>
+        
 
         <!-- footer -->
         <TheFooter></TheFooter>
@@ -13,8 +15,14 @@
 <script>
 import TheNavbar from '../components/TheNavbar.vue';
 import TheFooter from '../components/TheFooter.vue';
+import Home from '../pages/Home.vue';
+import Contact from '../pages/Contact.vue';
+import About from '../pages/About.vue';
 
     export default {
-    components: { TheNavbar, TheFooter }
+    components: { TheNavbar, TheFooter, Home, Contact, About },
+    data() {
+        
+    }
 }
 </script>
